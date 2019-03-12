e7362c3a5e0b835b46b36f156e384d861bf88146
vasile <elsile69@yahoo.com>
2019-03-12 16:36:23 +0200

    simple elements

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "jquery";
+// import "bootstrap/dist/js/bootstrap";
+import "./css/normalize.css";
+import "./css/skeleton.css";
+import "./css/style.css";
 
-import App from "./App";
+import HomePage from "./home";
+import Store from "./store";
+import Cart from "./cart";
+
 const routing = (
   <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
-      <Route path="/" component={App} />
+    <div className="container">
+      <nav class="navigation">
+        <ul className="row">
+          <li className="four columns">
+            <Link to="/">Home</Link>
+          </li>
+          <li class="four columns">
+            <Link to="/store">Shopping</Link>
+          </li>
+          <li class="four columns">
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path="/" component={HomePage} />
+      <Route path="/store" component={Store} />
+      <Route path="/cart" component={Cart} />
     </div>
   </Router>
 );
